841e10a3b4fc60cdb458d9c98afa5a7502bcdd60
DESKTOP-acer <takgue1224@gmail.com>
2021-10-12 15:22:05 +0900

    연산자 완료

--- operator.js
+++ operator.js
@@ -34,14 +34,55 @@
 // console.log(`count: ${count}, postIncrement: ${postIncrement}`) // count: 2, postIncrement: 1
 
 
-const shirtsPrice = 100000
-const pantsPrice = 80000
-let totalPrice = 0
+// const shirtsPrice = 100000
+// const pantsPrice = 80000
+// let totalPrice = 0
 
-totalPrice += shirtsPrice // totalPrice = totalPrice + shirtsPrice 와 동일
-console.log(totalPrice)
-totalPrice += pantsPrice // totalPrice = totalPrice + pantsPrice 와 동일 
-console.log(totalPrice)
+// totalPrice += shirtsPrice // totalPrice = totalPrice + shirtsPrice 와 동일
+// console.log(totalPrice)
+// totalPrice += pantsPrice // totalPrice = totalPrice + pantsPrice 와 동일 
+// console.log(totalPrice)
 
-totalPrice -= shirtsPrice // totalPrice = totalPrice - shirtsPrice 와 동일
-console.log(totalPrice) 
+// totalPrice -= shirtsPrice // totalPrice = totalPrice - shirtsPrice 와 동일
+// console.log(totalPrice) 
+
+
+// console.log(1 < 2) // 1이 2보다 작은가? true
+// console.log(2 <= 2) // 2가 2보다 작거나 같은가? true
+// console.log(1 > 2) // 1이 2보다 큰가? false
+// console.log(1 >= 2) // 1이 2보다 크거나 같은가? false
+
+
+// let isOnSale = true
+// let isDiscountItem = true
+
+// console.log(isOnSale && isDiscountItem) // true && true 이므로 true
+// console.log(isOnSale || isDiscountItem) // true || true 이므로 true
+
+// isOnSale = false
+// console.log(isOnSale && isDiscountItem) // false && true 이므로 false
+// console.log(isOnSale || isDiscountItem) // false || true 이므로 true
+
+// isDiscountItem = false
+// console.log(isOnSale && isDiscountItem) // false && false 이므로 false
+// console.log(isOnSale || isDiscountItem) // false || false 이므로 false
+
+// console.log(!isOnSale) // !false 이므로 true
+
+
+// console.log(1 === 1) // true
+// console.log(1 === 2) // false
+// console.log('Javascript' === 'Javascript') // true
+// console.log('Javascript' === 'javascript') // 대소문자나 띄워쓰기도 다 정확히 일치해야 해요. 따라서 false
+
+// // == / === 차이점 알아보기 
+// console.log(1 === "1") // false를 출력
+// console.log(1 == "1" // true를 출력
+
+// //Quiz 
+// const shoesPrice = 200000
+// const capPrice = 100000
+// const totalPrice = shoesPrice + capPrice
+
+// consoel.log(`총 ${totalPrice * 0.8}원에 물건을 구입합니다.`)
+
