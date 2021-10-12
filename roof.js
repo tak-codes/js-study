878161765fe427779dab58aeb76e0d335e43b64b
DESKTOP-acer <takgue1224@gmail.com>
2021-10-12 16:08:50 +0900

    반복문 완료

---
+++ roof.js
@@ -0,0 +1,27 @@
+// let temperature = 20
+// while (temperature < 25) {
+//     console.log(`${temperature} 도 정도면 적당한 온도입니다. `)
+//     temperature++
+// }   
+
+
+// for (let a = 20; a < 25; a++) {
+//     console.log(`${a}도 정도면 적당한 온도입니다.`)
+// }
+
+
+// for (let number = 1; number <= 10; number++) {
+// 	if (number % 3 === 0) {
+// 		console.log(`${number}는 3으로 나눠서 떨어지는 숫자입니다.`)
+// 	}
+// }
+
+
+Quiz
+for (let a = 1; a <=20; a++) {
+    if (a % 2 ===0) {
+        console.log(`${a}는 짝수입니다.`)
+    } else {
+        console.log(`${a}는 홀수입니다.`)
+    }
+}
