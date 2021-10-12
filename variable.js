df4fb4e940e536508a291b9d1b8a6c1adc58ef5b
DESKTOP-acer <takgue1224@gmail.com>
2021-10-12 14:24:44 +0900

    변수 선언까지

---
+++ variable.js
@@ -0,0 +1,40 @@
+// console.log(10)
+
+// let age = 37
+// console.log(age)
+
+// let name = 'Tak'
+// console.log(name) // 10 37 Tak
+
+
+// let isMan = true
+// let isWoman = false
+
+// console.log(isMan)
+// console.log(isWoman)
+// console.log(true)
+// console.log(false) // true false true false
+
+
+// let name = null
+// console.log(name)
+
+// let age
+// console.log(age) // null undefined
+
+//quiz.
+
+const brandName = 'Nike'
+const modelName = 'Air Jordan'
+console.log(brandName)
+console.log(modelName)
+
+const shoesPrice = 200000
+const capPrice = 180000
+console.log(shoesPrice)
+console.log(capPrice) // Nike Air Jordan 200000 180000
+
+
+
+
+
